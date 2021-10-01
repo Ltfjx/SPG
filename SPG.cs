7c399b6509194b6a3fa7730e3cbe5f2a57284405
Ltfjx <ltfjx2333@gmail.com>
2021-10-01 12:26:34 +0800

    fix bug :P

--- SPG.cs
+++ SPG.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 class SPG
 {
-    static string ReadNumber(){
+    static string ReadNumber()
+    {
         Console.Write("Please input a number for the length of your password:");
-        return Console.Read().ToString();
+        return Console.ReadLine().ToString();
     }
     static void Main()
     {
         string readString;
-	bool parseResult=false;
-	int numberOfChar=0;
-	while(!parseResult){
-            readString=ReadNumber();
-            parseResult=int.TryParse(readString, out numberOfChar);
+        bool parseResult = false;
+        int numberOfChar = 0;
+        while (!parseResult)
+        {
+            readString = ReadNumber();
+            parseResult = int.TryParse(readString, out numberOfChar);
         }
+        Console.WriteLine();
         string allchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
         char[] list = allchar.ToCharArray();
         Random rd = new Random();
-        for (int i = 0; i < numberOfChar-1; i++) { Console.Write(list[rd.Next(list.Length)]); }
+        for (int i = 0; i < numberOfChar; i++) { Console.Write(list[rd.Next(list.Length)]); }
         Console.ReadLine();
     }
 }
